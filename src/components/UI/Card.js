4596b34f4faad6b0ab772fe7ef7a5e52c86beab7
Konstantinos Angeletopoulos <kostasAngels@outlook.com>
2022-05-20 18:12:19 +0300

    Section 3: React Basics & Working With Components

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import './Card.css'
+
+const Card = (props) => {
+    const classses = 'card ' + props.className
+    return <div className={classses}>{props.children}</div>;
+
+}
+
+export default Card
