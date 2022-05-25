f4f3b5aa63a0efa935ef29d754a0335220e3164a
Kostas Angeletopoulos <kostas.angeletopoulos@aqurance.local>
2022-05-25 15:56:51 +0300

    Section 7: Debugging React Apps

--- s7/src/components/CourseGoals/CourseInput/CourseInput.js
+++ s7/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,38 +3,37 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import styles from './CourseInput.module.css';
 
-
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
-    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
-        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
-    </form >
+    </form>
   );
 };
-
 
 export default CourseInput;
