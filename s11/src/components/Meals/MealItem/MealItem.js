75bada4e906a2b3b69b44cbcd10cc38a6fd69430
Konstantinos Angeletopoulos <kostasAngels@outlook.com>
2023-04-07 11:36:06 +0300

    Section 11: Practice Project: Building a Food Order App

--- s11/src/components/Meals/MealItem/MealItem.js
+++ s11/src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,19 @@
-import classes from './MealItem.module.css';
+import { useContext } from 'react';
 import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
+
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
     const price = `€${props.price.toFixed(2)}`;
+    const addToHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
     return (
         <li className={classes.meal}>
             <div>
@@ -10,7 +22,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={addToHandler} />
             </div>
 
         </li>
